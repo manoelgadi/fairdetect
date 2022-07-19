0098c4aaaf021352a1f47b1b9df2ddb9a40deb15
manoelgadi <manoelgadi@gmail.com>
2022-07-19 10:27:47 +0200

    uploading my first python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fairdetect',
+      version='0.5',  # Development release
+      description='Library to identify bias in pre-trained models!',
+      url='https://github.com/manoelgadi/fairdetect',
+      author='Prof. Manoel Gadi',
+      author_email='mfalonso@faculty.ie.edu',
+      license='MIT',
+          packages=['fairdetect'],
+      zip_safe=False)
